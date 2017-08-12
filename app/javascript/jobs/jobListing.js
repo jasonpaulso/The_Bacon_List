d736f6a03fb62d6bfb2b983ad09304d34260edc1
Jason Southwell <jason@jasonpaulsouthwell.com>
2017-08-12 14:47:42 -0700

    fix: added conditional loading of job list to handle zero jobs

--- app/javascript/jobs/jobListing.js
+++ app/javascript/jobs/jobListing.js
@@ -7,21 +7,27 @@ class JobListing extends Component {
 
   render = () => {
     const { jobs } = this.props
-    return (
-      <div>
-        {jobs && jobs.length &&
-          jobs.map((job, i) => {
-            return (
-              <div key={i} style={{border: "1px solid black"}}> 
-                <h4 style={{borderBottom: "1px solid black", textAlign: "center"}}>{job.title} <Link to={{ pathname: `/form/${job.id}`}} style={{fontSize: "10px"}}>Edit Job</Link></h4>
-                <p>{job.description}</p>
-                
-              </div>
-            )
-          })
-        }
-      </div>
-    )
+
+    if (jobs.length) {
+      return (
+        <div>
+          {jobs && jobs.length &&
+            jobs.map((job, i) => {
+              return (
+                <div key={i} style={{border: "1px solid black"}}> 
+                  <h4 style={{borderBottom: "1px solid black", textAlign: "center"}}>{job.title} <Link to={{ pathname: `/form/${job.id}`}} style={{fontSize: "10px"}}>Edit Job</Link></h4>
+                  <p>{job.description}</p>
+                  
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+    } else {
+      return null
+    }
+
   }
 
 }
