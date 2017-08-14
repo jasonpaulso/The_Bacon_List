a4250d1ed854b85581707e0b30749f7f3e8777d3
Jason Southwell <jason@jasonpaulsouthwell.com>
2017-08-14 12:11:59 -0700

    style: styling job list

--- app/javascript/jobs/jobListing.js
+++ app/javascript/jobs/jobListing.js
@@ -3,21 +3,36 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link, Switch, withRouter } from 'react-router-dom'
 import EditJob from './editJob'
 import GoogleMapReact from 'google-map-react'
+import Moment from 'moment';
 
 class JobListing extends Component {
+
+
+  renderDate(date) {
+    Moment.locale('en')
+    return(<span> {Moment(date).format('M.D.Y')} </span>) //basically you can do all sorts of the formatting and others
+  }
 
   render = () => {
     const { jobs } = this.props
 
     if (jobs.length) {
       return (
-        <div>
+        <div style={{width: "100%"}}>
           {jobs && jobs.length &&
+              
             jobs.map((job, i) => {
               return (
-                <div key={i} style={{border: "1px solid black"}}> 
-                  <h4 style={{borderBottom: "1px solid black", textAlign: "center"}}>{job.title} <Link to={{ pathname: `/form/${job.id}`}} style={{fontSize: "10px"}}>Edit Job</Link></h4>
-                  <p>{job.description}</p>
+                <div key={i} className="job-row-container"> 
+                  <div className="job-listing-logo-container"></div>
+                  <div className="job-preview-detail-container">
+                    {this.renderDate(job.created_on)}
+                    <h4>{job.title}</h4>
+                    <h4>Google</h4>
+                  </div>
+                  <div className="job-preview-detail-show-container">
+                    <Link to={`/form/${job.id}`} style={{fontSize: "10px"}}>Edit Job</Link>
+                  </div>
                 </div>
               )
             })
--- app/javascript/jobs/listJobs.js
+++ app/javascript/jobs/listJobs.js
@@ -51,7 +51,7 @@ class App extends Component {
 
   renderJobsList(jobs) {
     if (jobs.length) {
-      return <div><JobListing jobs={jobs}/></div>
+      return <div style={{ width: "100%"}}><JobListing jobs={jobs}/></div>
     } else {
       return <div><h1>Sorry, no jobs for your search. Perhaps try being less specific.</h1></div>
     }
@@ -75,28 +75,44 @@ class App extends Component {
 
     if (query) {
       const match = new RegExp(escapeRegExp(query), 'i')
-      showingJobs = Array.from(new Set(jobsCollection.filter((job) => match.test(job.title)).concat(jobsCollection.filter((job) => match.test(job.description))))) 
+
+      const titleFilter = (job) => match.test(job.title)
+      const descriptionFilter = (job) => match.test(job.description)
+
+      showingJobs = Array.from(new Set(jobsCollection.filter(titleFilter).concat(jobsCollection.filter(descriptionFilter)))) 
     } else {
       showingJobs = jobsCollection
     }
 
-    showingJobs.sort(sortBy:"title")
-
+    // showingJobs.sort(sortBy:"title")
+  
     return (
       <Switch>
         <Route exact path="/" render={({history}) => (
-
-          <div className="container"><h1>Job List | <Link to={{ pathname: '/form', query: { updateJobList: e => this.props.updateJobList(e) } }} style={{fontSize: ""}} >Add Job</Link></h1>
-            <div>
+          <div className="main">
+          <div className="row header" ><h1>The Bacon</h1>
+          <div className="header-right">
             <input
             className='search-jobs'
             type='text'
-            placeholder='Search Jobs'
+            placeholder='Search:'
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <Link to={"/form"} className="add-job-button">Add Listing</Link>
+
+
           </div>
+            
+          </div>
+          
+          <div className="row jobs-list">
+            <div className="jobs-list-header">
+              <span>Listings found: {jobsCollection.length}</span>
+              <span>Sort by: Date added</span>
+            </div>
             {jobsCollection && showingJobs && this.renderJobsList(showingJobs)}
+          </div>
           </div>
         )}/>
         <Route path="/form/:jobId?" render={JobForm}/> 
